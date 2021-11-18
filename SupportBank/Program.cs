478bf94bfa781a77f5f0d74cccd4a9bcc95be50e
Alex Yip <alex.yip@softwire.com>
2021-11-18 10:58:43 +0000

    Read CSV

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualBasic.FileIO;
 
 namespace SupportBank
 {
@@ -6,7 +8,53 @@ namespace SupportBank
     {
         static void Main(string[] args)
         {
-            
+            var transactions = new List<Transaction>();
+            using (TextFieldParser parser = new TextFieldParser(@"C:\work\training\SupportBank\Transactions2014.csv"))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                // Skip header line
+                parser.ReadLine();
+                while (!parser.EndOfData)
+                {
+                    //Processing row
+                    string[] fields = parser.ReadFields();
+                    transactions.Add(new Transaction(fields));
+                }
+            }
+
+            foreach (var transaction in transactions)
+            {
+                Console.Out.WriteLine(transaction.From);
+            }
+        }
+    }
+
+    class Transaction
+    {
+        public DateTime Date { get; }
+        public string From { get; }
+        public string To { get; }
+        public string Narrative { get; }
+        public double Amount { get; }
+
+        public Transaction(DateTime date, string from, string to, string narrative, double amount)
+        {
+            Date = date;
+            From = from;
+            To = to;
+            Narrative = narrative;
+            Amount = amount;
+        }
+
+        public Transaction(string[] csvFields) : this(
+            DateTime.ParseExact(csvFields[0], "d", null),
+            csvFields[1],
+            csvFields[2],
+            csvFields[3],
+            double.Parse(csvFields[4])
+        )
+        {
         }
     }
 }
